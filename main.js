3da57b9ba2e03429a35c917d2a57b3051f575122
Sae Matsuda <sae.matsuda.843@gmail.com>
2024-04-09 13:25:58 +0900

    added a function to the navbar

--- main.js
+++ main.js
@@ -8,3 +8,23 @@ window.addEventListener("load", function () {
   preloader.classList.add("loaded");
   document.body.classList.add("loaded");
 });
+
+// Add eventListener to multiple elements
+const addEventOnElements = function (elements, eventType, callback) {
+  elements.forEach((element) => {
+    element.addEventListener(eventType, callback);
+  });
+};
+
+// Navbar
+const navBar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navBar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
