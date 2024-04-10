bb41c26228cdb09455b15c02813de4282cfbf6b7
Sae Matsuda <sae.matsuda.843@gmail.com>
2024-04-10 10:56:07 +0900

    added an event to the header

--- main.js
+++ main.js
@@ -34,4 +34,21 @@ const header = document.querySelector("[data-header]");
 
 let lastScrollPos = 0;
 
-const hideHeader = function () {};
+const hideHeader = function () {
+  const isScrollBottom = lastScrollPos < window.scrollY;
+  if (isScrollBottom) {
+    header.classList.add("hide");
+  } else {
+    header.classList.remove("hide");
+  }
+  lastScrollPos = window.scrollY;
+};
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 50) {
+    header.classList.add("active");
+    hideHeader();
+  } else {
+    header.classList.remove("active");
+  }
+});
